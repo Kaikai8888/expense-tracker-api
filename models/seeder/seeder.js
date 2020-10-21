fa5bd70e9d9998b7bc15fe003ff57a99c222eb6d
Kaikai8888 <wuky812@gmail.com>
2020-10-21 18:28:11 +0800

    refactor: simplify seeder.js code with insertMany

--- models/seeder/seeder.js
+++ models/seeder/seeder.js
@@ -5,15 +5,13 @@ const Record = require('../record.js')
 const records = require('../data/records.js')
 
 db.once('open', () => {
-  categories.default.forEach(category => {
-    Category.create(category)
-      .then(category => records.forEach(record => {
-        if (record.category !== category.name) return
-        record.category = category._id
-        Record.create(record)
-          .catch(error => console.error(error))
-      }))
-      .catch(error => console.error(error))
-  })
+  Category.insertMany(categories.default)
+    .then(categories => records.forEach(record => {
+      record.category = categories.find(category => category.name === record.category)._id
+      Record.create(record)
+        .catch(error => console.error(error))
+    }))
+    .catch(error => console.error(error))
+
   console.log('Complete seed data creation.')
 })
