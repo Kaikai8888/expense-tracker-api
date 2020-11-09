7f5e20100342697d78f0f790a7da53f5a6fc5366
Kaikai8888 <wuky812@gmail.com>
2020-11-09 21:07:50 +0800

    feat: modify seeder

--- models/data/records.js
+++ models/data/records.js
@@ -2,34 +2,81 @@ module.exports = [{
   name: '房租',
   category: '家居物業',
   date: new Date(2020, 3, 5),
-  amount: 10000
+  amount: 10000,
+  userId: 'user1'
 },
 {
   name: '高鐵',
   category: '交通出行',
   date: new Date(2020, 3, 10),
-  amount: 1000
+  amount: 1000,
+  userId: 'user1'
 },
 {
   name: '看電影',
   category: '休閒娛樂',
   date: new Date(2020, 3, 21),
-  amount: 700
+  amount: 700,
+  userId: 'user1'
 },
 {
   name: '午餐',
   category: '餐飲食品',
   date: new Date(2020, 3, 11),
-  amount: 168
+  amount: 168,
+  userId: 'user1',
 },
 {
   name: '晚餐',
   category: '餐飲食品',
   date: new Date(2020, 3, 26),
-  amount: 350
+  amount: 350,
+  userId: 'user1',
 },
 {
   name: '捐款',
   category: '其他',
-  amount: 3000
+  amount: 3000,
+  userId: 'user1'
+},
+{
+  name: '管理費',
+  category: '家居物業',
+  date: new Date(2020, 3, 5),
+  amount: 1000,
+  userId: 'user2'
+},
+{
+  name: '高鐵',
+  category: '交通出行',
+  date: new Date(2020, 3, 10),
+  amount: 1000,
+  userId: 'user2'
+},
+{
+  name: '泡溫泉',
+  category: '休閒娛樂',
+  date: new Date(2020, 3, 21),
+  amount: 700,
+  userId: 'user2'
+},
+{
+  name: '午餐',
+  category: '餐飲食品',
+  date: new Date(2020, 3, 11),
+  amount: 168,
+  userId: 'user2',
+},
+{
+  name: '晚餐',
+  category: '餐飲食品',
+  date: new Date(2020, 3, 26),
+  amount: 350,
+  userId: 'user2',
+},
+{
+  name: '繳稅',
+  category: '其他',
+  amount: 3000,
+  userId: 'user2'
 }]
--- models/seeder/seeder.js
+++ models/seeder/seeder.js
@@ -1,16 +1,42 @@
+const bcrypt = require('bcryptjs')
 const db = require('../../config/mongoose.js')
 const Category = require('../category.js')
-const categories = require('../data/categories.json')
 const Record = require('../record.js')
+const User = require('../user.js')
+const { categories } = require('../data/categories.json')
+const { users } = require('../data/users.json')
 const records = require('../data/records.js')
 
+
 db.once('open', () => {
-  Category.insertMany(categories.default)
-    .then(categories => records.forEach(record => {
-      record.category = categories.find(category => category.name === record.category)._id
-      Record.create(record)
-        .catch(error => console.error(error))
-    }))
+  Promise.all([
+    Category.deleteMany(),
+    User.deleteMany(),
+    Record.deleteMany()
+  ])
+    .then(() => Promise.all([
+      Category.insertMany(categories),
+      Promise.all(users.map(user =>
+        bcrypt
+          .genSalt(10)
+          .then(salt => bcrypt.hash(user.password, salt))
+          .then(hash => {
+            user.password = hash
+            return User.create(user)
+          })
+      ))
+    ]))
+    .then(results => {
+      records.forEach(record => {
+        record.category = results[0].find(category => category.name === record.category)._id
+        record.userId = results[1].find(user => user.name === record.userId)._id
+      })
+      return Record.insertMany(records)
+    })
+    .then(() => {
+      console.log('Complete seed data creation.')
+      process.exit()
+    })
     .catch(error => console.error(error))
-  console.log('Complete seed data creation.')
 })
+
