2458020a521ffa5b3bbed63b3a24cc38d58c7bb3
Kaikai8888 <wuky812@gmail.com>
2020-11-09 21:37:11 +0800

    feat: add login and register page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home.js')
 const records = require('./modules/records.js')
+const users = require('./modules/users.js')
 
-router.use('/', home)
+router.use('/users', users)
 router.use('/records', records)
+router.use('/', home)
 
 module.exports = router
