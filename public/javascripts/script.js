9077059f319039d674fea255f598733698b85f04
Kaikai8888 <wuky812@gmail.com>
2020-11-11 23:07:12 +0800

    feat: limit filter function to by year and month
    not allowing users to filter by just year or just month

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,13 +4,40 @@ const forms = document.querySelectorAll('form')
 const timestampInput = document.querySelector('form #timestamp')
 const dateInput = document.querySelector('form #date')
 const dateFields = document.querySelectorAll('.date-field')
-const deleteForms = document.querySelectorAll('.delete-form')
 
 
 //Index page
-//Filter by category 
-if (filterForm && categorySelect) {
+//Filter 
+if (filterForm) {
   categorySelect.addEventListener('change', event => filterForm.submit())
+  //limit filtering function to by a combination of year and month, not just by month or by year
+  filterForm.addEventListener('change', event => {
+    const year = document.querySelector('#year-select')
+    const month = document.querySelector('#month-select')
+    if (event.target.id === 'year-select' && year.value !== 'all' &&
+      month.value === 'all') {
+      month.value = '1'
+    } else if (event.target.id === 'month-select' && month.value !== 'all' &&
+      year.value === 'all') {
+      console.log(year.options.length)
+      if (year.options.length > 1) {
+        year.value = year.options[year.options.length - 1].value
+      } else {
+        month.value = 'all'
+      }
+    } else if (event.target.id !== 'category-select' && event.target.value === 'all') {
+      month.value = 'all'
+      year.value = 'all'
+    }
+    console.log(year.selectedIndex)
+    console.log(month.selectedIndex)
+  })
+  // Manage time zone difference
+  // pass client side timezone info back to server for year and month filtering
+  // filterForm.addEventListener('submit', event => {
+  //   const timezoneOffset = document.querySelector('#timezone-offset')
+  //   timezoneOffset.value = new Date().getTimezoneOffset()
+  // })
 }
 
 // Manage time zone difference
