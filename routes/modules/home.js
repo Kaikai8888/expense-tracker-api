4629d3307d18b3fca5d5d67b038938318a6db2cd
Kaikai8888 <wuky812@gmail.com>
2020-11-12 11:56:36 +0800

    feat: add year and month filtering hide category filtering function temporary for debugging

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,38 +4,53 @@ const Category = require('../../models/category.js')
 const Record = require('../../models/record.js')
 
 router.get('/', (req, res) => {
-  const { category, year, month, utcOffset } = req.query
-  console.log('@@1: ', category, year, month, utcOffset)
   const userId = req.user._id
+  const { category, year, month } = req.query
+  let { utcOffset } = req.query
+  const yearMonthFilter = {}
+
+  console.log('@@1: ', category, year, month, utcOffset)
+
+  if (year && month && utcOffset) {
+    //transfer to MongoDB UTC offset format
+    utcOffset = utcOffset > 0 ? '+' + utcOffset : utcOffset
+    utcOffset = Math.abs(utcOffset) < 10 ? utcOffset[0] + '0' + utcOffset[1] : utcOffset
+    console.log('@@2 utcOffset: ', utcOffset)
+    yearMonthFilter.$and = [
+      { $eq: [{ $year: { date: '$date', timezone: utcOffset } }, Number(year)] },
+      { $eq: [{ $month: { date: '$date', timezone: utcOffset } }, Number(month)] }
+    ]
+  }
 
   Category.find()
     .lean()
     .then(categories => {
-      if (selectedCategory !== 'all' && !categories.some(category => category.name === selectedCategory)) return res.render('error', { errorMessage: 'Cannot find this category.' })
+      //find selected category id
+      const categoryDoc = categories.find(item => item.name === category)
+      const categoryId = categoryDoc ? categoryDoc._id : undefined
 
-      return Record.find({ userId })
+      return Record
+        .find({
+          userId,
+          // category: categoryId,
+          $expr: yearMonthFilter
+        })
         .populate('category')
         .sort({ date: 'asc' })
         .lean()
         .then(records => {
-          let totalAmount = 0
-          const filterRecords = records.filter(record => {
-            if (category !== undefined && category !== 'all' && record.category.name !== category) return false
-            const year = Number(record.date.getFullYear())
-            totalAmount += record.amount
+          let totalAmount = records.reduce((sum, record) => {
+            sum += record.amount
             record.date = record.date.getTime()
-            return true
-          })
-          // console.log('@@2: ', records)
-          // console.log('@@3: ', filterRecords)
-          res.render('index', {
+            return sum
+          }, 0)
+          return res.render('index', {
             category, year, month,
-            categories, totalAmount,
-            records: filterRecords,
+            records, categories, totalAmount,
             isHome: true
           })
         })
-    )
+    })
     .catch(error => console.error(error))
 })
 
