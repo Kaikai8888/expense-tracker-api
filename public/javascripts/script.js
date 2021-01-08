7fdaddfa4239ee3ce08a298dee66e342a8acdba7
Kaikai8888 <wuky812@gmail.com>
2021-01-08 22:22:20 +0800

    feat: remove redundant front end javascript

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,27 +5,20 @@ const sort = document.querySelector('#sort')
 const month = document.querySelector('#month')
 const dateFields = document.querySelectorAll('.date-field')
 const clearFilterBtn = document.querySelector('.clear-filter')
-// const clearYearMonth = document.querySelector('#clear-year-month')
-// const sortBtns = document.querySelectorAll('#sort-btns button')
-
 //new and edit page
 const forms = document.querySelectorAll('form')
 const timestampInput = document.querySelector('form #timestamp')
 const dateInput = document.querySelector('form #date')
 
-
-
 //Index page
 //Filter 
 if (filterForm) {
   [category, month, sort].forEach(node => node.addEventListener('change', event => {
-    // ensureYearMonthFilter() //user can only filter by a combination of year and month
     setUTCOffset() // Manage time zone difference
     filterForm.submit()
   }))
 
   filterForm.addEventListener('submit', () => {
-    // ensureYearMonthFilter()
     setUTCOffset()
   })
 
@@ -36,40 +29,7 @@ if (filterForm) {
     search.value = ''
     filterForm.submit()
   })
-
-  // clearYearMonth.addEventListener('click', event => {
-  //   document.querySelector('#year').value = ''
-  //   document.querySelector('#month').value = ''
-  //   setUTCOffset() // Manage time zone difference
-  //   filterForm.submit()
-  // })
-
-  // sortBtns.forEach(btn => btn.addEventListener('click', function onSortBtnsClick(event) {
-  //   const sort = document.querySelector('#sort')
-  //   switch (this.dataset.sort) {
-  //     case 'amount-asc':
-  //       sort.value = 'amount-desc'
-  //       break
-  //     case 'amount-desc':
-  //       sort.value = 'amount-asc'
-  //       break
-  //     case 'date-asc':
-  //       sort.value = 'date-desc'
-  //       break
-  //     case 'date-desc':
-  //       sort.value = 'date-asc'
-  //       break
-  //     default:
-  //       sort.value = 'date-asc'
-  //       break
-  //   }
-  //   ensureYearMonthFilter()
-  //   setUTCOffset()
-  //   filterForm.submit()
-  // }))
-
 }
-
 
 // Manage time zone difference
 // Modify client side input: transfer date string into timestamp, and store in hidden input field
@@ -99,7 +59,6 @@ $(function () {
 
 
 //----function----//
-
 function setUTCOffset() {
   const utcOffset = document.querySelector('#utc-offset')
   let offset = - (new Date().getTimezoneOffset() / 60)
@@ -108,14 +67,5 @@ function setUTCOffset() {
   utcOffset.value = offset
 }
 
-// function ensureYearMonthFilter() {
-//   const year = document.querySelector('#year')
-//   const month = document.querySelector('#month')
-//   if (!year.value && month.value) {
-//     year.value = new Date().getFullYear()
-//   } else if (!month.value && year.value) {
-//     month.value = '1'
-//   }
-// }
 
 
